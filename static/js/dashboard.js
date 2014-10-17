4dab9aecc80819891ebbc9faf0d41bd840718967
Andrew Mussey <MusseyAM@vt.edu>
2014-10-17 12:09:52 -0400

    Added resizing to the status circles.

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
     var url = "http://localhost:5000/api/v1/monitors/?small";
-    if (username != undefined) {
-        url = "http://localhost:5000/api/v1/monitors/" + username + "?small";
-    }
+    // if (username != undefined) {
+    //     url = "http://localhost:5000/api/v1/monitors/" + username + "?small";
+    // }
 
     $.get(url, function(data) {
         response = JSON.parse(data);
@@ -16,50 +16,83 @@ $(document).ready(function() {
             for (var i = 0; i < response[entry].values.length; i++) {
                 $("#checks").append(
                     "<div style=\"width:200px; height:120px; float:left; font-size:10px; margin-bottom: 20px;\">\n" +
-                    "    <canvas height=\"100\" width=\"100\" id=\"status-" + response[entry].values[i].id + "\" style=\"float:left; margin: 0 50px 0 50px; width:100px; height:100px;\"></canvas>\n" +
+                    "    <div style=\"height:100px; width:100px; margin: 0 50px 0 50px; \"><canvas height=\"100\" width=\"100\" id=\"status-" + response[entry].values[i].id + "\" style=\"float:left; width:100px; height:100px;\"></canvas></div>\n" +
                     "    <div style=\"width:200px; height:20px; float:left;\"><center>" + response[entry].values[i].server_name + /* "<br>" + response[entry].values[i].hostname + */ "</center></div>\n" +
                     "</div>");
-                drawCircle("status-" + response[entry].values[i].id, response[entry].values[i].alarms.length, response[entry].values[i].status);
+                statusCircle("status-" + response[entry].values[i].id, response[entry].values[i].status);
             }
         });
     });
 });
 
 
-function drawCircle(id, alerts, status) {
-    var canvas = document.getElementById(id);
-    if (canvas.getContext) {
+function statusCircle(id, status) {
+    if (status.good == undefined) {
+        status.good = 0;
+    }
+    if (status.warning == undefined) {
+        status.warning = 0;
+    }
+    if (status.bad == undefined) {
+        status.bad = 0;
+    }
 
-        var ctx = canvas.getContext("2d"); 
+    window.addEventListener("resize", function(){ drawCircle(id, status); }, false);
+    drawCircle(id, status);
+}
+
+function drawCircle(id, status) {
+    var canvas = document.getElementById(id);
+    canvas.width = canvas.parentElement.offsetWidth
+    canvas.height = canvas.parentElement.offsetHeight
+
+
+    if (canvas.getContext) {
+        cuts = status.good + status.warning + status.bad;
+
+        var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
         var x = canvas.width/2;
         var y = canvas.height/2;
-        var radius = (canvas.width > canvas.height ? canvas.height/2 : canvas.width/2);
+        var radius = (x > y ? y : x)-1;
         var startAngle = 0;
         var endAngle = Math.PI*2;
         var antiClockwise = false;
 
-        var passing_tests = status['good'];
+        ctx.beginPath();
+        ctx.arc(x, y, radius+1, 0, Math.PI*2, antiClockwise);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.fillStyle = '#000'; // '#ff0';
+        ctx.fill();
 
-        for (var i = 0; i < alerts; i++) {
-            startAngle = 0 + (((Math.PI*2)/alerts)*(i-1));
-            endAngle = ((Math.PI*2)/alerts)*i;
-            
-            var color = "#" +
-                Math.floor((Math.random() * 15) + 1).toString(16) +
-                Math.floor((Math.random() * 15) + 1).toString(16) +
-                Math.floor((Math.random() * 15) + 1).toString(16);
+        var colorings = {good: 0, warning: 0, bad: 0};
+
+        for (var i = 0; i < cuts; i++) {
+            startAngle = 0 + (((Math.PI*2)/cuts)*(i-1));
+            endAngle = ((Math.PI*2)/cuts)*i;
+
+            var color = "#ff311b";
+            if (status.good > colorings.good) {
+                colorings.good++;
+                color = "#03dc18";
+            } else if (status.warning > colorings.warning) {
+                colorings.warning++;
+                color = "#f8ff33";
+            }
 
             ctx.beginPath();
             ctx.arc(x, y, radius, startAngle, endAngle, antiClockwise);
             ctx.lineTo(x, y);
             ctx.closePath();
-            ctx.fillStyle = getStatusColor(status); //color; // '#ff0';
+            ctx.fillStyle = color;
             ctx.fill();
         }
 
         ctx.beginPath();
         ctx.arc(x, y, radius-(radius/5), 0, Math.PI*2, antiClockwise);
-        ctx.lineTo(75, 75);
+        ctx.lineTo(x, y);
         ctx.closePath();
         ctx.fillStyle = '#fff'; // '#ff0';
         ctx.fill();
@@ -67,18 +100,7 @@ function drawCircle(id, alerts, status) {
         var fontHeight = (radius*0.55)
         ctx.font = 'bold ' + fontHeight + 'px Arial';
         ctx.fillStyle = '#000';
-        var txt = passing_tests + '/' + alerts;
+        var txt = cuts + '/' + cuts;
         ctx.fillText(txt, x-ctx.measureText(txt).width/2, y+(fontHeight/3.0));
     }
-}
-
-function getStatusColor(status) {
-    if (status.good > 0) {
-        status.good--;
-        return "#03dc18"; //#00c212";
-    } else if (status.warning > 0) {
-        status.warning--;
-        return "#f8ff33";
-    }
-    return "#ff311b"; //#de1c07";
 }
